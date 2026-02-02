31db08f5bb4c9296477ca08246e3558c5283697e
Diego Personal (via WeWeb) <miguel.yanpo@gmail.com>
2026-02-02 15:46:19 -0500

    v458 - 3240

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 457;
+const version = 458;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
