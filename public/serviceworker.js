12091a49043e4dfe431eef720d80062aedae561f
Miguel Paez (via WeWeb) <miguel.yanpo@gmail.com>
2026-04-13 10:31:55 -0500

    v888 - 4732

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 887;
+const version = 888;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
