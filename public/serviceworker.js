1c60d15af116e95a006f63f1b671afe8205db3a7
Miguel Paez (via WeWeb) <miguel.yanpo@gmail.com>
2026-04-22 08:46:12 -0500

    v950 - 5040

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 949;
+const version = 950;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
